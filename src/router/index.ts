7706f5df57c5fc4300d126c68734e771170af103
tom <yangzihao@sunngs.com>
2025-09-17 17:42:16 +0800

    feat: 🎸 登录页结构和样式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const publicRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index.vue'),
+  },
+]
+
 const routes = []
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: publicRoutes,
 })
 
 export default router
