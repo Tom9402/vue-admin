c3229298ce1c28712b4088ff41bfca68fa135234
tom <yangzihao@sunngs.com>
2025-10-31 17:51:42 +0800

    feat: 🎸 5-09：方案落地：国际化缓存处理

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useLanguageStore } from './stores/language'
+
+const languageStore = useLanguageStore()
 </script>
 
 <template>
-  <RouterView />
+  <el-config-provider :locale="languageStore.language === 'en' ? 'en' : 'zhCn'">
+    <RouterView />
+  </el-config-provider>
 </template>
 
 <style scoped></style>
