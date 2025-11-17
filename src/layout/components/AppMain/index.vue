d3bd607e3bc123a6f4c0e2d05760a5e42ae4be84
tom <yangzihao@sunngs.com>
2025-11-17 11:32:18 +0800

    feat: 🎸 5-31：方案落地：创建 tags 数据源

--- src/layout/components/AppMain/index.vue
+++ src/layout/components/AppMain/index.vue
@@ -4,7 +4,27 @@
   </div>
 </template>
 
-<script></script>
+<script setup lang="ts">
+import { useAppStore } from '@/stores/app'
+import { generateTitle } from '@/utils/i18n'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const appStore = useAppStore()
+
+// 生成 title
+const getTitle = (route: { path: string; meta: { title: string } }) => {
+  let title = ''
+  if (!route.meta) {
+    // 处理无 meta 的路由
+    const pathArr = route.path.split('/')
+    title = pathArr[pathArr.length - 1]
+  } else {
+    title = generateTitle(route.meta.title)
+  }
+  return title
+}
+</script>
 
 <style lang="scss" scoped>
 .app-main {
---
+++ src/stores/app.ts
@@ -0,0 +1,20 @@
+import { TAGS_VIEW } from '@/constant'
+import { getStorageItem, setStorageItem } from '@/utils/storage'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useAppStore = defineStore('app', () => {
+  const tagsViewList = ref(getStorageItem(TAGS_VIEW) || [])
+
+  const addTagsViewList = (tag: { path: string; name: string }) => {
+    const isFind = tagsViewList.value.find((item: { path: string }) => item.path === tag.path)
+    // 处理重复
+    if (isFind) {
+      return
+    }
+    tagsViewList.value.push(tag)
+    setStorageItem(TAGS_VIEW, tagsViewList.value)
+  }
+
+  return { tagsViewList, addTagsViewList }
+})
