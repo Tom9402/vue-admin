af3fad26b382c7916b222f9021d2ea7544327cf8
Tom <yangzihao@sunngs.com>
2026-08-25 17:38:16 +0800

    feat: 个人中心-作者

--- src/views/profile/components/Author.vue
+++ src/views/profile/components/Author.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>Author</div>
+  <div class="author-container">
+    <div class="header">
+      <pan-thumb
+        image="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+        height="60px"
+        width="60px"
+        :hoverable="false"
+      >
+        {{ $t('msg.profile.name') }}
+      </pan-thumb>
+      <div class="header-desc">
+        <h3>{{ $t('msg.profile.name') }}</h3>
+        <span>{{ $t('msg.profile.job') }}</span>
+      </div>
+    </div>
+    <div class="info">
+      {{ $t('msg.profile.Introduction') }}
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const props = defineProps({
-  project: {
-    type: Object,
-    default: () => ({}),
-  },
-})
+import PanThumb from './PanThumb.vue'
 </script>
+
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
