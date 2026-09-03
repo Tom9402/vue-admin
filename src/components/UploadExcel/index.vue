3780445731d4b8fd97857682e7654734cf71652f
Tom <yangzihao@sunngs.com>
2026-09-03 11:36:58 +0800

    feat: 导入页面

---
+++ src/components/UploadExcel/index.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="upload-excel">
+    <div class="btn-upload">
+      <el-button :loading="loading" type="primary" @click="handleUpload">
+        {{ $t('msg.uploadExcel.upload') }}
+      </el-button>
+    </div>
+
+    <input
+      ref="excelUploadInput"
+      class="excel-upload-input"
+      type="file"
+      accept=".xlsx, .xls"
+      @change="handleChange"
+    />
+    <!-- https://developer.mozilla.org/zh-CN/docs/Web/API/HTML_Drag_and_Drop_API -->
+    <div
+      class="drop"
+      @drop.stop.prevent="handleDrop"
+      @dragover.stop.prevent="handleDragover"
+      @dragenter.stop.prevent="handleDragover"
+    >
+      <i class="el-icon-upload" />
+      <span>{{ $t('msg.uploadExcel.drop') }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import XLSX from 'xlsx'
+import { ref, defineProps } from 'vue'
+
+const props = defineProps({
+  // 上传前的回调函数
+  beforeUpload: {
+    type: Function,
+    default: () => {}
+  },
+  // 成功回调函数
+  onSuccess: {
+    type: Function,
+    default: () => {}
+  }
+})
+
+// 点击上传触发
+const loading = ref(false)
+const excelUploadInput = ref(null)
+// 上传文件
+const handleUpload = () => {
+  excelUploadInput.value.click()
+}
+
+// 根据导入内容，生成数据
+const generateData = (excelData) => props.onSuccess && props.onSuccess(excelData)
+
+// 获取表头
+const getHeaderRow = (worksheet) => {
+  const header = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+  return header[0]
+}
+
+// 读取数据（异步）
+const readerData = (rawFile) => {
+  loading.value = true
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      // 1. 获取解析到的数据
+      const data = e.target.result
+      // 2. 使用 XLSX 解析数据
+      const workbook = XLSX.read(data, { type: 'array' })
+      // 3. 获取第一张表格（工作簿）名称
+      const firstSheetName = workbook.SheetNames[0]
+      // 4. 只读取 Sheet1 的数据
+      const worksheet = workbook.Sheets[firstSheetName]
+      // 5. 解析数据表头
+      const header = getHeaderRow(worksheet)
+      // 6. 解析数据体
+      const results = XLSX.utils.sheet_to_json(worksheet)
+      // 7. 传入解析后的数据
+      generateData({ header, results })
+      loading.value = false
+      resolve()
+    }
+    // 启动读取文件内容
+    reader.readAsArrayBuffer(rawFile)
+  })
+}
+
+// 触发上传事件
+const upload = (rawFile) => {
+  excelUploadInput.value.value = null
+  // 如果没有指定上传前回调的话
+  if (!props.beforeUpload) {
+    readerData(rawFile)
+    return
+  }
+  // 如果指定了上传前回调的话，只有返回true才上传文件
+  if (props.beforeUpload(rawFile)) {
+    readerData(rawFile)
+  }
+}
+
+const handleChange = (e) => {
+  const rawFile = e.target.files[0]
+  if (!rawFile) return
+  upload(rawFile)
+}
+</script>
+
+<style lang="scss" scoped>
+.upload-excel {
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+  .excel-upload-input {
+    display: none;
+    z-index: -9999;
+  }
+  .btn-upload,
+  .drop {
+    border: 1px dashed #bbb;
+    width: 350px;
+    height: 160px;
+    text-align: center;
+    line-height: 160px;
+  }
+  .drop {
+    line-height: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #bbb;
+    i {
+      font-size: 60px;
+      display: block;
+    }
+  }
+}
+</style>
--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup lang="ts">
-import UploadExcel from '@/components/UploadExcel'
+import UploadExcel from '@/components/UploadExcel/index.vue'
 </script>
