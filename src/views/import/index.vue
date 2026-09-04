c1db7570116e63e3660cd20a5d68cb158993caac
Tom <yangzihao@sunngs.com>
2026-09-04 17:14:32 +0800

    feat(提交表格解析数据): 提交表格解析数据

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -3,10 +3,38 @@
 </template>
 
 <script setup lang="ts">
+import { userBatchImport } from '@/api/user-manage'
 import UploadExcel from '@/components/UploadExcel/index.vue'
+import router from '@/router'
+import { USER_RELATIONS } from '@/views/import/utils'
+import { ElMessage } from 'element-plus'
+import { useI18n } from 'vue-i18n'
 
+const i18n = useI18n()
+
+/*
+ * 筛选数据
+ */
+const generateData = (results) => {
+  const arr = []
+  results.forEach((item) => {
+    const userInfo = {}
+    Object.keys(item).forEach((key) => {
+      userInfo[USER_RELATIONS[key]] = item[key]
+    })
+    arr.push(userInfo)
+  })
+  return arr
+}
 // 数据解析成功后的回调
-const onSuccess = (excelData) => {
-  console.log(excelData)
+const onSuccess = async ({ header, results }) => {
+  const data = generateData(results)
+  // 8. 调用批量导入接口
+  await userBatchImport(data)
+  ElMessage.success({
+    message: results.length + i18n.t('msg.excel.importSuccess'),
+    type: 'success'
+  })
+  router.push('/user/manage')
 }
 </script>
